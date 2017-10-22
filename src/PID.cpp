8213e3a13a10cd0888c456b9fd373aa8c17ef378
Ioannis Tornazakis <itornaza@gmail.com>
2017-10-22 21:53:47 +0300

    refactor: Checking code style

--- src/PID.cpp
+++ src/PID.cpp
@@ -2,20 +2,23 @@
 
 using namespace std;
 
-/*
-* TODO: Complete the PID class.
-*/
-
 PID::PID() {}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  // TODO: Initialize proportional error
+  // TODO: Initialize differential error
+  // TODO: Initialize integral error
 }
 
 void PID::UpdateError(double cte) {
+  // TODO: Update p_error
+  // TODO: Update i_error
+  // TODO: Update d_error
 }
 
 double PID::TotalError() {
+  // TODO: Calculate total PID error
+  // TODO: Return total PID error
 }
-
